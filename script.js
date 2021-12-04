340bcd423dc30a14d695e2721b10597708ddabe9
talha <talha.yuecel@student.htldornbirn.at>
2021-12-05 00:25:30 +0100

    erste Aufgabe java vom Video erledigt

---
+++ script.js
@@ -0,0 +1,33 @@
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+// Berechnungen und console.log
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result);
+
+result = (a + b) / c;
+console.log("Das Ergebnis ist " + result);
+
+result = c - a + b;
+console.log("Das Ergebnis ist " + result);
+
+result = a + b + c * 3;
+console.log("Das Ergebnis ist " + result);
+
+result = c - a + b;
+console.log("Das Ergebnis ist " + result);
+
+result = a + b + c / 4;
+console.log("Das Ergebnis ist " + result);
+
+// Variablenänderung
+a = 3;
+b = a + 3;
+c = c - 3;
+
+// Die neue Wertenausgabe
+console.log("A hat den Wert " + a);
+console.log("A hat den Wert " + b);
+console.log("A hat den Wert " + c);
