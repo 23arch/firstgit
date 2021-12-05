1923c794aa363e3f00876ba8513e6dbbf4601913
talha <talha.yuecel@student.htldornbirn.at>
2021-12-05 15:59:13 +0100

    Java zweite aufgabe gemacht

---
+++ E_secondjava/script.js
@@ -0,0 +1,26 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+// Texte STRINGS
+
+let name = "Asd";
+console.log(name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
+
+a = 17.4;
+b = 3.9;
+result = a / b;
+console.log("The result of " + a + " divided by " + b + " is " + result)
+
+let firstname = "Talha"
+let lastname = "Yücel"
+console.log("Ich heiße " + firstname + " " +
+    lastname)
