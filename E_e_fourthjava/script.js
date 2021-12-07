9eb87377c276fc09d5cc58d479a195b53897d831
talha <talha.yuecel@student.htldornbirn.at>
2021-12-07 21:59:43 +0100

    Java Aufgabe 4 gemacht (Schleifen)

---
+++ E_e_fourthjava/script.js
@@ -0,0 +1,41 @@
+let a = 7;
+let b = 8;
+let name = "TEST";
+
+console.log(name + a + b);
+
+// Schleife
+
+for (let count = 0; count < 10; count++) {
+    console.log(count + " TEST")
+}
+
+
+// Zähle von 7 bis 45
+for (let count = 7; count <= 45; count++) {
+    console.log(count + " erste Aufgabe")
+}
+// Zähle von 9 bis 99
+for (let count = 9; count <= 99; count++) {
+    console.log(count + " zweite Aufgabe")
+}
+// Zähle rückwärts von 10 bis 0
+for (let count = 10; count >= 0; count--) {
+    console.log(count + " dritte Aufgabe")
+}
+// Zähle rückwärts von 1234 bis 1207
+for (let count = 1234; count >= 1207; count--) {
+    console.log(count + " vierte Aufgabe")
+}
+// Gib Deinen Namen genau 8 mal aus
+for (let count = 0; count <= 8; count++) {
+    console.log(count + " Talha")
+}
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (let count = 0; count <= 10; count++) {
+    console.log(count)
+}
+console.log("Talha")
+for (let count = 10; count >= 0; count--) {
+    console.log(count)
+}
