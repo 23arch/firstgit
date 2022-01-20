a33efbb438f681844de4daec12d67a2a4bb731dd
talha <talha.yuecel@student.htldornbirn.at>
2022-01-20 14:57:29 +0100

    Test geschrieben Name(ZZZTest) Beginn 14:15 Ende 14:55

---
+++ ZZZTest/script.js
@@ -0,0 +1,10 @@
+console.log("Ich zähle von 1 bis 8")
+
+for (let count = 1; count <= 8; count++) {
+    console.log(count)
+}
+
+console.log("Ich zähle in Zweier-Schritten Rückwärts")
+for (let count = 11; count >= -3; count = count - 2) {
+    console.log(count)
+}
